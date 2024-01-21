fcd7241ba205404dea2d42eec9f38bbe312bc6ea
Muhammad Ahmed <ahmedsidd_qui@outlook.com>
2024-01-21 17:19:17 +0500

    finalize

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+document.getElementById("navsection").innerHTML = `
+<div class="top-section">
+<div class="logo">
+  <a href="/assets/page/home.html"><img src="/assets/images/logo.png" alt=""></a>
+</div>
+<div class="nav-items">
+  <ul>
+    <li><a href="/assets/page/home.html">home</a></li>
+    <li><a href="/assets/page/resume.html">resume</a></li>
+    <li><a href="/assets/page/blogs.html">blogs</a></li>
+    <li><a href="/assets/page/about.html">about</a></li>
+  </ul>
+</div>
+</div>
+`
+document.getElementById("footersection").innerHTML = `
+<footer>
+<p>&copy; 2024, allaboutahmed</p>
+</footer>`
+// about.html form
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    location.reload;
+    alert("Your form has been submitted successfully!");
+  });
+  
+  
