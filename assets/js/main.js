474242e232b9b1dbc6b49b54b32926a7506d3e9c
Muhammad Ahmed <ahmedsidd_qui@outlook.com>
2024-01-23 22:18:17 +0500

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 document.getElementById("navsection").innerHTML = `
 <div class="top-section">
 <div class="logo">
-  <a href="/assets/page/home.html"><img src="/assets/images/logo.png" alt=""></a>
+  <a href="index.html"><img src="/assets/images/logo.png" alt=""></a>
 </div>
 <div class="nav-items">
   <ul>
-    <li><a href="/assets/page/home.html">home</a></li>
-    <li><a href="/assets/page/resume.html">resume</a></li>
-    <li><a href="/assets/page/blogs.html">blogs</a></li>
-    <li><a href="/assets/page/about.html">about</a></li>
+    <li><a href="home.html">home</a></li>
+    <li><a href="resume.html">resume</a></li>
+    <li><a href="blogs.html">blogs</a></li>
+    <li><a href="about.html">about</a></li>
   </ul>
 </div>
-<div class="navicon"><a href="#"><i class="fa-solid fa-bars fa-lg" style="color: #666666;"></i></a></div>
+<div class="navicon"><i class="fa-solid fa-bars fa-lg navicon" style="color: #666666;"></i></div>
 </div>
 `
 document.getElementById("footersection").innerHTML = `
