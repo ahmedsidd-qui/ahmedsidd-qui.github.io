b3bc9e3e415a11545890e8b9bb2bd7fc4e672282
Muhammad Ahmed <148879105+ahmedsidd-qui@users.noreply.github.com>
2024-07-21 06:53:55 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 document.getElementById("navsection").innerHTML = `
 <div class="top-section">
 <div class="logo">
-  <a href="index.html"><img src="/assets/images/logo.png" alt=""></a>
+  <a href="./"><img src="/assets/images/logo.png" alt=""></a>
 </div>
 <div class="nav-items">
   <ul>
-    <li><a href="home.html">home</a></li>
-    <li><a href="resume.html">resume</a></li>
-    <li><a href="work.html">work</a></li>
-    <li><a href="blogs.html">blogs</a></li>
-    <li><a href="about.html">about</a></li>
+    <li><a href="./home">home</a></li>
+    <li><a href="./resume">resume</a></li>
+    <li><a href="./work">work</a></li>
+    <li><a href="./blogs">blogs</a></li>
+    <li><a href="./about">about</a></li>
   </ul>
 </div>
 </div>
